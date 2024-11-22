b772159b8c8cd8b0888348e51cfecbffca54a5cd
wangmr <mingrui@whut.edu.cn>
2024-11-22 19:02:55 +0800

    实现了导出数据的功能

--- src/api/order/index.js
+++ src/api/order/index.js
@@ -2,21 +2,18 @@
  * @Author: wangmr mingrui@whut.edu.cn
  * @Date: 2024-11-22 10:57:53
  * @LastEditors: wangmr mingrui@whut.edu.cn
- * @LastEditTime: 2024-11-22 18:25:05
+ * @LastEditTime: 2024-11-22 18:53:08
  * @FilePath: /BigHealth/BigHealthMarket_FrontEnd/src/api/order/index.js
  * @Description: 
  * 2405499352@qq.com
  */
 import request from '@/utils/request'
+
 export const getOrders=(data)=>{
     return request.get('/order/list',{ 
         params: data
     }
 )}
-export const exportOrderList=(data)=>{
-   
-}
-
 
 export const approveRefundRequest=(id)=>{
     return request.get('/orderDetail/approveRefund',{
--- src/views/order/indent.vue
+++ src/views/order/indent.vue
@@ -5,25 +5,22 @@
       <el-form-item label="姓名">
         <el-input v-model="filters.name" placeholder="请输入姓名" />
       </el-form-item>
+      <!--
       <el-form-item label="性别">
         <el-select v-model="filters.gender" placeholder="请选择" style="width: 100px;">
           <el-option label="男" value="男"></el-option>
           <el-option label="女" value="女"></el-option>
         </el-select>
       </el-form-item>
+      -->
       <el-form-item label="手机号">
         <el-input v-model="filters.phone" placeholder="请输入手机号" />
       </el-form-item>
       <el-form-item label="选择日期">
-        <el-date-picker
-          v-model="filters.date"
-          type="date"
-          placeholder="请选择日期"
-          style="width: 200px;"
-        />
+        <el-date-picker v-model="filters.date" type="date" placeholder="请选择日期" style="width: 200px;" />
       </el-form-item>
       <el-form-item>
-        <el-button type="primary" @click="fetchOrders">查询</el-button>       
+        <el-button type="primary" @click="fetchOrders">查询</el-button>
         <el-button @click="resetFilters">重置</el-button>
       </el-form-item>
     </el-form>
@@ -34,13 +31,7 @@
     </div>
 
     <!-- 订单列表 -->
-    <el-table
-      :data="orders"
-      border
-      v-loading="loading"
-      style="width: 100%"
-      @selection-change="handleSelectionChange"
-    >
+    <el-table :data="orders" border v-loading="loading" style="width: 100%" @selection-change="handleSelectionChange">
       <el-table-column type="selection" />
       <el-table-column prop="id" label="序号" align="center" width="80" />
       <el-table-column prop="orderNumber" label="订单号" align="center" />
@@ -51,34 +42,22 @@
       <el-table-column prop="price" label="付款价格" align="center" />
       <el-table-column prop="status" label="状态" align="center">
         <template #default="{ row }">
-          <el-tag
-            :type="getStatusType(row.status)"
-            disable-transitions
-          >
+          <el-tag :type="getStatusType(row.status)" disable-transitions>
             {{ row.status }}
           </el-tag>
         </template>
       </el-table-column>
-      <el-table-column prop="orderTime" label="下单时间" align="center" >
+      <el-table-column prop="orderTime" label="下单时间" align="center">
         <template #default="{ row }">
           <span>{{ formatDateTime(row.orderTime) }}</span>
         </template>
       </el-table-column>
       <el-table-column prop="refund" label="退款" align="center">
         <template #default="{ row }">
-          <el-button
-            v-if="row.status === '申请退款'"
-            size="small"
-            type="primary"
-            @click="approveRefund(row)"
-          >
+          <el-button v-if="row.status === '申请退款'" size="small" type="primary" @click="approveRefund(row)">
             同意退款
           </el-button>
-          <el-tag
-            v-if="row.status === '退款成功'"
-            size = "small"
-            type="success"
-          >
+          <el-tag v-if="row.status === '退款成功'" size="small" type="success">
             退款成功
           </el-tag>
           <span v-else>{{ row.refund }}</span>
@@ -87,24 +66,19 @@
     </el-table>
 
     <!-- 分页器 -->
-    <el-pagination
-      background
-      layout="prev, pager, next, sizes, total"
-      :current-page="pagination.currentPage"
-      :page-size="pagination.pageSize"
-      :total="pagination.total"
-      style="margin-top:20px"
-      @current-change="handlePageChange"
-      @size-change="handleSizeChange"
-    />
+    <el-pagination background layout="prev, pager, next, sizes, total" :current-page="pagination.currentPage"
+      :page-size="pagination.pageSize" :total="pagination.total" style="margin-top:20px"
+      @current-change="handlePageChange" @size-change="handleSizeChange" />
   </div>
 </template>
 
 <script setup>
 import { ref, reactive } from "vue";
-import { getOrders, exportOrderList, approveRefundRequest } from "@/api/order";
+import { getOrders, approveRefundRequest } from "@/api/order";
 import { ElMessage } from "element-plus";
-import {formatDateTime } from '@/utils/convert'
+import * as XLSX from "xlsx";
+import { saveAs } from "file-saver";
+import { formatDateTime } from '@/utils/convert'
 const filters = reactive({
   name: "",
   gender: "",
@@ -199,6 +173,49 @@ const approveRefund = async (row) => {
     ElMessage.error("退款失败");
   }
 };
+const exportOrderList = (data) => {
+    if (orders.value.length === 0) {
+      ElMessage.warning("没有数据可导出");
+      return;
+    }
+
+    // 格式化导出数据
+    const exportData = orders.value.map((order,index) => ({
+      序号: index+1,
+      订单号: order.orderNumber,
+      姓名: order.name,
+      性别: order.gender,
+      手机号: order.phone,
+      地址信息: order.address,
+      商品名称: order.productName,
+      付款价格: order.price,
+      状态: order.status,
+      下单时间: formatDateTime(order.orderTime),
+    }));
+
+    // 创建 Excel 表格
+  const worksheet = XLSX.utils.json_to_sheet(exportData); // 转换为工作表
+  const workbook = XLSX.utils.book_new(); // 创建新工作簿
+  XLSX.utils.book_append_sheet(workbook, worksheet, "订单数据"); // 将工作表添加到工作簿
+
+  // 生成 Excel 文件 Blob
+  const excelBuffer = XLSX.write(workbook, {
+    bookType: "xlsx",
+    type: "array",
+  });
+  const blob = new Blob([excelBuffer], {
+    type: "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet",
+  });
+
+  // 下载 Excel 文件
+  saveAs(blob, `订单数据_${new Date().toISOString().slice(0, 10)}.xlsx`);
+
+  ElMessage.success("订单数据已成功导出");
+  };
+
+
+
+
 
 fetchOrders();
 </script>
